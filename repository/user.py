5e0b1159fe9cef8c0d673854ba3b76f05adc8c9e
SaeediAlireza <alirexasaeedi@gmail.com>
2025-06-04 15:46:09 +0330

    user

--- repository/user.py
+++ repository/user.py
@@ -1,16 +1,67 @@
-from sqlalchemy.sql import exists
+from datetime import datetime
 from sqlalchemy.orm import Session
-from model import model
+from model.model import *
+from model.schemas import *
+from fastapi import HTTPException
 
 
-def user_exist(db: Session, user: model.User):
-    return db.query(
-        model.User.query.filter(model.User.user_name == user.user_name).exists()
-    ).scalar()
-
-
-def add_user(db: Session, new_user: model.User):
-    db.add(new_user)
+def create_proposal(db: Session, proposal: ProposalRequest):
+    db_proposal = Proposal(
+        info=proposal.info,
+        RFP_id=proposal.RFP_id,
+        user_id=proposal.user_id,
+        file_id=proposal.file_id,
+        state=proposal.state,
+        comment=proposal.comment,
+    )
+    db.add(db_proposal)
     db.commit()
-    db.refresh(new_user)
-    return new_user
+    db.refresh(db_proposal)
+    return db_proposal
+
+
+def get_proposal_by_id(db: Session, proposal_id: int):
+    proposal = db.query(Proposal).filter(Proposal.id == proposal_id).first()
+    if not proposal:
+        raise HTTPException(status_code=404, detail="Proposal not found")
+    return proposal
+
+
+def get_projects(
+    db: Session,
+    user_id: int,
+    skip: int = 0,
+    limit: int = 10,
+):
+
+    return (
+        db.query(Project)
+        .filter(Project.user_user_id == user_id)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+
+
+def get_reports_by_project(db: Session, project_id: int):
+    return db.query(Report).filter(Report.project_id == project_id).all()
+
+
+def create_report(db: Session, report: ReportRequest):
+    db_report = Report(
+        info=report.info,
+        project_id=report.project_id,
+        comment=report.comment,
+        state=report.state,
+    )
+    db.add(db_report)
+    db.commit()
+    db.refresh(db_report)
+    return db_report
+
+
+def get_report_by_id(db: Session, report_id: int):
+    report = db.query(Report).filter(Report.id == report_id).first()
+    if report is None:
+        raise HTTPException(status_code=404, detail="Report not found")
+    return report
--- routers/user.py
+++ routers/user.py
@@ -1,13 +1,49 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status, Response
-from model import model, schemas
+
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import Session
-from sqlalchemy import update, desc
 from datetime import time
 
 from repository.user import *
-from util import util
+from util.util import *
 
 
 router = APIRouter(tags=["user"], prefix="/users")
+
+
+@router.post("/proposals/", response_model=Proposal)
+async def add_proposal(
+    proposal_request: ProposalRequest, db: Session = Depends(get_db)
+):
+    return create_proposal(db=db, proposal=proposal_request)
+
+
+@router.get("/proposals/{proposal_id}", response_model=Proposal)
+async def read_proposal(proposal_id: int, db: Session = Depends(get_db)):
+    return get_proposal_by_id(db=db, proposal_id=proposal_id)
+
+
+@router.get("/projects/", response_model=List[Project])
+async def read_projects(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    user_id = 0
+    projects = get_projects(db, skip=skip, limit=limit, user_id=user_id)
+    return projects
+
+
+@router.get("/reports-by-project/{project_id}", response_model=List[Report])
+async def read_reports(project_id: int, db: Session = Depends(get_db)):
+    reports = get_reports_by_project(db, project_id)
+    if not reports:
+        raise HTTPException(status_code=404, detail="Reports not found")
+    return reports
+
+
+@router.post("/reports/", response_model=Report)
+async def add_report(report_request: ReportRequest, db: Session = Depends(get_db)):
+    return create_report(db=db, report=report_request)
+
+
+@router.get("/reports/{report_id}", response_model=Report)
+async def read_report(report_id: int, db: Session = Depends(get_db)):
+    return get_report_by_id(db=db, report_id=report_id)
