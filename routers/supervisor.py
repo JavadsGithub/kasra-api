b4cdc4dc6a007ba460e2fe40c38a8f8ebc6097f4
alirexasaeedi <alirexasaeedi@gmail.com>
2025-09-15 17:55:33 +0330

    change 4

--- routers/supervisor.py
+++ routers/supervisor.py
@@ -124,3 +124,21 @@ async def read_projects(
     if not projects:
         raise HTTPException(status_code=404, detail="No Projects found")
     return projects
+
+
+@router.put("/user/{user_id}", response_model=UserInfoResponse)
+async def change_password(
+    current_user: Annotated[schemas.UserInfoResponse, Depends(get_current_user)],
+    user_id: int,
+    new_password: str,
+    db: Session = Depends(get_db),
+):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="user not found")
+    user.password = hash(password=new_password)
+
+    db.commit()
+    db.refresh(user)
+    return user
