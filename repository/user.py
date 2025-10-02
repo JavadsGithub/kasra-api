dfdc150fbd03757609037248e229384129148668
alirexasaeedi <alirexasaeedi@gmail.com>
2025-10-02 07:18:01 +0330

    debug user filter

--- repository/user.py
+++ repository/user.py
@@ -124,11 +124,11 @@ def create_notif(db: Session, user_id: int, title: str):
 def admin_get_users_like(
     db: Session, active: bool, skip: int = 0, limit: int = 10, fname: str = None
 ):
-    query = db.query(User).order_by(User.id.desc())
+    query = db.query(User).order_by(
+        User.id.desc()).filter(User.active == active)
     if fname:
         query = query.filter(User.lname.ilike(
             f"%{fname}%"))
-    query.filter(User.active == active)
     return query.offset(skip).limit(limit).all()
 
 
